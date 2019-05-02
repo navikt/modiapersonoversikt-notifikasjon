9f59875096027559a6736f863875a40722633d43
Richard Borge <Richard.Borge@nav.no>
2019-05-02 13:15:46 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktorVersion = "1.1.3"
+val prometheusVersion = "0.4.0"
+val logbackVersion = "1.2.3"
+val logstashVersion = "5.1"
+val amazonS3Version = "1.11.534"
+val konfigVersion = "1.6.10.0"
+
+val mainClass = "no.nav.modiapersonoversikt.ApplicationKt"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.21"
+}
+
+buildscript {
+    dependencies {
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.2.0")
+    }
+}
+
+application {
+    mainClassName = mainClass
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    compile("io.ktor:ktor-server-netty:$ktorVersion")
+    compile("io.ktor:ktor-gson:$ktorVersion")
+    compile("io.prometheus:simpleclient_hotspot:$prometheusVersion")
+    compile("io.prometheus:simpleclient_common:$prometheusVersion")
+    compile("ch.qos.logback:logback-classic:$logbackVersion")
+    compile("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    compile("com.amazonaws:aws-java-sdk:$amazonS3Version")
+    compile("com.natpryce:konfig:$konfigVersion")
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven("https://dl.bintray.com/kotlin/ktor")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.3.1"
+}
+
+
+tasks.named<Jar>("jar") {
+    baseName = "app"
+
+    manifest {
+        attributes["Main-Class"] = mainClass
+        attributes["Class-Path"] = configurations["compile"].joinToString(separator = " ") {
+            it.name
+        }
+    }
+
+    doLast {
+        configurations["compile"].forEach {
+            val file = File("$buildDir/libs/${it.name}")
+            if (!file.exists())
+                it.copyTo(file)
+        }
+    }
+}
+
+tasks.named<KotlinCompile>("compileKotlin") {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.named<KotlinCompile>("compileTestKotlin") {
+    kotlinOptions.jvmTarget = "1.8"
+}
